53c5c91f4e5be2912ae799d97ca3f91f0c27ea50
TrippBishopStats <dyne_erg@yahoo.com>
2023-12-25 21:39:45 -0700

    added infor for normal, binomial, geometric, and exponential distributions.

--- probability-distributions.R
+++ probability-distributions.R
@@ -14,6 +14,8 @@ theme_set(theme_minimal())
 
 ## Uniform Distribution
 
+# The R nickname for the uniform distribution is "unif".
+
 # The random draw function
 unif_data <- runif(1000000, 0, 10)
 
@@ -72,3 +74,112 @@ runif(5,1,5)
 
 set.seed(3)
 runif(5,1,5)
+
+## The Normal Distribution
+
+# The R nickname for the normal distribution is "norm".
+
+normally_distributed <- rnorm(1000000, mean=0, sd=1)
+
+# create a data frame containing density data for normal distribution
+norm_frame <- with(density(normally_distributed), data.frame(x,y))
+
+# Get prob of observing a value less than -1
+prob_under_minus1 <- pnorm(q=-1,        
+                           mean=0,
+                           sd=1)
+
+# Get prob of observing a value over 1
+prob_over_1 <-  1-pnorm(q=1,            
+                        mean=0,
+                        sd=1)
+
+# Prob between -1 and 1
+between_prob <- 1-(prob_under_minus1+prob_over_1) 
+
+norm_frame |> 
+  ggplot(aes(x=x,y=y)) +
+    geom_line() +
+    geom_ribbon(data=subset(norm_frame, x < -1), 
+                aes(ymax=y, ymin=0), 
+                fill="red", 
+                alpha=0.4) +
+    geom_ribbon(data=subset(norm_frame, x > 1),
+                aes(ymax=y,ymin=0),
+                fill="red",
+                alpha=0.4) +
+    geom_ribbon(data = subset(norm_frame, x > -1 & x < 1),
+                aes(ymax=y,ymin=0),
+                fill="skyblue",
+                alpha=0.4) +
+    geom_text(x=-1.6,y=0.03, label=round(prob_under_minus1, 4), size=4) +
+    geom_text(x=1.6,y=0.03, label=round(prob_over_1, 4), size=4) +
+    geom_text(x=0,y=0.1, label=round(between_prob, 4), size=4) +
+    xlim(-4,4)
+
+
+qnorm(p = 0.025)      # Find the quantile at the 2.5% cutoff
+qnorm(p = 0.975)      # Find the quantile at the 97.5% cutoff
+
+
+## Binomial Distribution
+
+# The R nickname for the Binomial distribution is "binom"
+fair_coin_flips <- rbinom(1000000, # generate draws from binomal dist
+                          size=10, # with 10 trials
+                          prob=0.5) # success probability
+
+table(fair_coin_flips)
+
+hist(fair_coin_flips, breaks=seq(-0.5, 10.5, 1))
+
+# Note how similar this result is to the normal distribution. This is the result
+# of the probability of success being 0.5. The normal distribution approximates
+# the binomial distribution to a fair degree of accuracy when n*p > 10 and
+# n(1-p) > 10. The normal distribution to use is one where mu = np and sigma =
+# sqrt(np(1-p)).
+
+# If the probability is not 0.5, then there will be skewness in the
+# distribution.
+
+unfair_coin_flips <- rbinom(1000000, # generate draws from binomal dist
+                          size=10, # with 10 trials
+                          prob=0.8) # success probability
+
+hist(unfair_coin_flips, breaks=seq(-0.5, 10.5, 1))
+
+# Notice here how the results are skewed in favour of more successes. For
+# example, it is extremely unlikely that 0 successes will be realised in 10
+# trials of this unfair coin.
+
+## Geometric distribution - It's a cousin of the binomial distribution. It is
+## also a discrete distribution and it models the amount of time it takes for
+## the first successful trial to occur given a specificed probability of
+## success. The nickname of the distribution in R is "geom".
+
+set.seed(12)
+flips_til_heads <- rgeom(n=1000000, prob=0.5) + 1
+
+table(flips_til_heads)
+
+hist(flips_til_heads, breaks=seq(-0.5, max(flips_til_heads)+0.5, 1))
+
+# Here, we have counted the results from 1M experiments and tallied the number
+# of Bernoulli trials required to get the first head in each experiment.
+
+# The exponential distribution is the continuous version of the geometric
+# distribution. It models the time until the first event based on a specified
+# occurrence rate. The nickname in R for the exponential distribution is "exp".
+
+exponential_data <- rexp(n=1000000, 
+                         rate=1)     # Success/arrival rate
+
+# Note that for the arrival rate, the mean arrival time is equal to 1/rate.
+
+prob_longer_1 <- pexp(q=1,
+                      rate=1,
+                      lower.tail = FALSE)
+
+prob_longer_1
+
+plot(density(exponential_data))
